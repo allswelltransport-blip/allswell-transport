121c2c9a452c0c2a2dcd30ae5d1cb46ff19a224c
allswelltransport-blip <allswelltransport@gmail.com>
2026-05-20 16:34:56 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-## 4. src/main.jsx
-```jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -10,4 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-```
